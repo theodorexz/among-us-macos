ecb1d7556be313b624446b6279634117f8f61156
theodorexz <117528721+theodorexz@users.noreply.github.com>
2026-06-27 10:44:21 -0400

    Fixing errors related with versions of next.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,5 @@ const nextra = require('nextra').default
 const withNextra = nextra({})
 
 module.exports = withNextra({
-  typescript: {
-    ignoreBuildErrors: true
-  }
+  reactStrictMode: true
 })
