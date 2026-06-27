5917059898094704a0b655d3e29a40a8e4b89741
theodorexz <117528721+theodorexz@users.noreply.github.com>
2026-06-27 10:36:05 -0400

    Refactor page.jsx to use optional chaining and update imports

--- app/[[...mdxPath]]/page.jsx
+++ app/[[...mdxPath]]/page.jsx
@@ -1,22 +1,23 @@
 import { generateStaticParamsFor, importPage } from 'nextra/pages'
-import { useMDXComponents as getMDXComponents } from '../../mdx-components'
+import { useMDXComponents } from '../../mdx-components'
 
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
 
 export async function generateMetadata(props) {
   const params = await props.params
-  const { metadata } = await importPage(params.mdxPath)
+  const { metadata } = await importPage(params?.mdxPath)
   return metadata
 }
 
-const Wrapper = getMDXComponents().wrapper
+const Wrapper = useMDXComponents().wrapper
 
 export default async function Page(props) {
   const params = await props.params
-  const result = await importPage(params.mdxPath)
-  const { default: MDXContent, toc, metadata } = result
+  const result = await importPage(params?.mdxPath)
+  const { default: MDXContent, metadata, sourceCode, toc } = result
+
   return (
-    <Wrapper toc={toc} metadata={metadata}>
+    <Wrapper metadata={metadata} sourceCode={sourceCode} toc={toc}>
       <MDXContent {...props} params={params} />
     </Wrapper>
   )
